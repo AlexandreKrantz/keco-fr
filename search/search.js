8c72f3f480bc627d04165e428337e561b842795c
AlexandreKrantz <alexandrekrantz@icloud.com>
2019-10-23 11:27:10 -0400

    Added "latest website files"

---
+++ search/search.js
@@ -0,0 +1,153 @@
+/* global instantsearch */
+
+app({
+  appId: 'JANRA19K71',
+  apiKey: 'a345491e56ba7434a97d791f379a5a0b',
+  indexName: 'Dev_BRANDS',
+  //appId: 'latency',
+  //apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  //indexName: 'instant_search',
+  searchParameters: {
+    hitsPerPage: 10,
+  },
+});
+
+function app(opts) {
+  // ---------------------
+  //
+  //  Init
+  //
+  // ---------------------
+  const search = instantsearch({
+    appId: opts.appId,
+    apiKey: opts.apiKey,
+    indexName: opts.indexName,
+    urlSync: true,
+    searchFunction: opts.searchFunction,
+  });
+
+  // ---------------------
+  //
+  //  Default widgets
+  //
+  // ---------------------
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#search-input',
+      placeholder: 'Products by keyword',
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#hits',
+      templates: {
+        item: getTemplate('hit'),
+        empty: getTemplate('no-results'),
+      },
+    })
+  );
+  search.addWidget(
+    instantsearch.widgets.stats({
+      container: '#stats',
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.sortBySelector({
+      container: '#sort-by',
+      autoHideContainer: true,
+      indices: [
+        {
+          name: opts.indexName,
+          label: 'Most relevant',
+        },
+        {
+          name: `${opts.indexName}_sustainability_desc`,
+          label: 'Most sustainable',
+        },
+      ],
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.pagination({
+      container: '#pagination',
+      scrollTo: '#search-input',
+    })
+  );
+
+  // ---------------------
+  //
+  //  Filtering widgets
+  //
+  // ---------------------
+  search.addWidget(
+    instantsearch.widgets.hierarchicalMenu({
+      container: '#hierarchicalCategories',
+      attributeName: 'categories',
+      limit: 100,
+      attributes: [
+        'hierarchicalCategories.lvl0',
+        'hierarchicalCategories.lvl1',
+        'hierarchicalCategories.lvl2',
+      ],
+      showParentLevel: true,
+      templates: {
+        header: getHeader('Categories'),
+        item:  '<a href="{{url}}" class="facet-item {{#isRefined}}active{{/isRefined}}"><span class="facet-name"><i class="fa fa-angle-right"></i> {{label}}</span class="facet-name"><span class="ais-hierarchical-menu--count">{{count}}</span></a>' // eslint-disable-line
+      },
+      collapsible: false,
+    })
+  );
+
+
+
+  search.addWidget(
+    instantsearch.widgets.menu({
+      container: '#location',
+      attributeName: 'location',
+      sortBy: ['isRefined', 'count:desc', 'name:asc'],
+      limit: 10,
+      showMore: true,
+      templates: {
+        header: getHeader('Location'),
+      },
+
+    })
+  );
+
+
+  search.start();
+}
+
+// ---------------------
+//
+//  Helper functions
+//
+// ---------------------
+function getTemplate(templateName) {
+  return document.querySelector(`#${templateName}-template`).innerHTML;
+}
+
+function getHeader(title) {
+  return `<h5>${title}</h5>`;
+}
+
+function getCategoryBreadcrumb(item) {
+  const highlightValues = item._highlightResult.categories || [];
+  return highlightValues.map(category => category.value).join(' > ');
+}
+
+function getStarsHTML(rating, maxRating) {
+  let html = '';
+  const newRating = maxRating || 5;
+
+  for (let i = 0; i < newRating; ++i) {
+    html += `<span class="ais-star-rating--star${
+      i < rating ? '' : '__empty'
+    }"></span>`;
+  }
+
+  return html;
+}
