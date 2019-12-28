5b5ecae59855e324a2aa972fb0b42fefd740d067
AlexandreKrantz <alexandrekrantz@icloud.com>
2019-12-28 16:35:11 +0100

    rows and cols for hit template

--- src/app.js
+++ src/app.js
@@ -24,16 +24,25 @@ instantsearch.widgets.hits({
     container: '#hits',
     templates: {
       item: `
-        <div>
-          <img src="{{image}}" alt="{{name}}" align="left" class="rounded img-fluid" />
-          <a href="{{BCORP_url}}"><div class="hit-rating badge badge-secondary">\{{Overall}}</div></a>
-
-          <div class="hit-name">
-            <h3>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h3>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-8">
+              <img src="{{image}}" alt="{{name}}" align="left" class="rounded img-fluid" />
+            </div>
+            
+            <div class="col-xs-4">
+              <a href="{{BCORP_url}}"><div class="hit-rating badge badge-secondary">\{{Overall}}</div></a>
+            </div>
           </div>
 
-          <div class="hit-description">
-            {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+          <div class="row">
+            <div class="hit-name">
+              <h3>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h3>
+            </div>
+
+            <div class="hit-description">
+              {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+            </div>
           </div>
         </div>
       `,
